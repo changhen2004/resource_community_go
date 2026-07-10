18df4cacefd0b27804ca5c92b2f52e2f2e47b260
changhen2004 <782190611@qq.com>
2026-07-10 14:56:44 +0800

    feat(router): add cors middleware support
    新增gin-contrib/cors中间件并配置跨域参数，允许前端localhost:5173地址发起的PUT、GET、POST等请求携带凭证

--- ExChangeAPP_backend/go.mod
+++ ExChangeAPP_backend/go.mod
@@ -3,6 +3,7 @@ module exchangeapp
 go 1.25.6
 
 require (
+	github.com/gin-contrib/cors v1.7.7
 	github.com/gin-gonic/gin v1.12.0
 	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/redis/go-redis/v9 v9.21.0
@@ -51,7 +52,7 @@ require (
 	go.mongodb.org/mongo-driver/v2 v2.5.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/arch v0.22.0 // indirect
+	golang.org/x/arch v0.23.0 // indirect
 	golang.org/x/net v0.56.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.40.0 // indirect
--- ExChangeAPP_backend/router/router.go
+++ ExChangeAPP_backend/router/router.go
@@ -3,20 +3,32 @@ package router
 import (
 	"exchangeapp/controllers"
 	"exchangeapp/middlewares"
-	
+	"time"
+
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetUpRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	auth := r.Group("/api/auth")
 	{
 		auth.POST("/login", controllers.Login)
 		auth.POST("/register", controllers.Register)
 	}
 	api := r.Group("/api")
-	
+
 	api.GET("/exchangeRates", controllers.GetExchangeRate)
 	api.Use(middlewares.AuthMiddleware())
 	{
@@ -24,7 +36,7 @@ func SetUpRouter() *gin.Engine {
 		api.POST("/articles", controllers.CreateArticle)
 		api.GET("/articles", controllers.GetArticles)
 		api.GET("/articles/:id", controllers.GetArticleByID)
-		
+
 		api.GET("/articles/:id/like", controllers.GetArticleLikes)
 		api.POST("/articles/:id/like", controllers.LikeArticle)
 	}
